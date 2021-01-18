27d4b37213c36415dd1c44685e02088b5b227ee6
Isaiah Anthony <isaiah.anthony99@gmail.com>
2021-01-17 21:17:55 -0800

    indexedDb stuff

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,6 +8,7 @@ self.addEventListener('install', event => {
           '/index.html',
           '/styles.css',
           'index.js',
+          '/db.js',
           '/icons/icon-192x192.png',
           '/icons/icon-512x512.png'
         ])
