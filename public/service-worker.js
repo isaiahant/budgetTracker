3a24252694483c63feed5dabb9872ab5dd70333d
Isaiah Anthony <isaiah.anthony99@gmail.com>
2021-01-20 20:11:24 -0800

    chart works

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,8 +7,8 @@ self.addEventListener('install', event => {
           '/',
           '/db.js',
           '/index.html',
-          'index.js',
-          '/manifest.json'
+          '/index.js',
+          '/manifest.json',
           '/styles.css',
           '/icons/icon-192x192.png',
           '/icons/icon-512x512.png'
@@ -18,15 +18,32 @@ self.addEventListener('install', event => {
 })
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(err => {
-        return caches.match(event.request).then(res => {
-            if (res) {
-              reurn res
-            } else if (event.request.headers.get('accept').includes('text/html')) {
-              return caches.match('/')
-            }
-          })
+  if (event.request.url.includes('/api/')){
+    event.respondWith(
+    caches.open('data-cache-v1').then(cache=>{
+      return fetch(event.request)
+      .then(response => {
+        if (response.status === 200) {
+          cache.put(event.request.url, response.clone())
+        }
+        return response
       })
-  )
+        .catch(err => cache.match(event.request))
+    }) .catch(err => console.log(err))
+    ) 
+    return 
+  }
+  event.respondWith(
+
+    fetch(event.request).catch(err => {
+      return caches.match(event.request).then(res => {
+        if (res) {
+          return res
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('/')
+        }
+      })
+    })
+    )
 })
+
